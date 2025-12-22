2ca04693a0d26f9deef01a975e8d01de7c4f112d
@eduardomarini <eduardo.marini@ativasolucoes.com.br>
2025-12-22 16:13:18 -0300

    FIX: medium

--- os_manager/db/migrations/004_create_service_orders.sql
+++ os_manager/db/migrations/004_create_service_orders.sql
@@ -4,7 +4,7 @@ CREATE TABLE service_orders (
     opened_by INT NOT NULL REFERENCES users(id),
     assigned_to INT REFERENCES users(id),
     status service_order_status NOT NULL DEFAULT 'open',
-    priority service_order_priority NOT NULL DEFAULT 'meduim',
+    priority service_order_priority NOT NULL DEFAULT 'medium',
     description TEXT NOT NULL,
     opened_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     closed_at TIMESTAMP
