3707c7e5a2e7776af1797bec7ccc6110b7e1ce69
@eduardomarini <eduardo.marini@ativasolucoes.com.br>
2025-12-22 17:34:33 -0300

    FIX: func GetUserByEmail

--- os_manager/internal/auth/repository.go
+++ os_manager/internal/auth/repository.go
@@ -25,3 +25,19 @@ func (r *Repository) CreateUser(
 	_, err := r.db.Exec(ctx, query, name, email, password, role)
 	return err
 }
+
+func (r *Repository) GetUserByEmail(ctx context.Context, email string) (
+	id int64,
+	hash string,
+	role string,
+	err error,
+) {
+	query := `
+		SELECT id, password, role
+		FROM users
+		WHERE email = $1
+	`
+
+	err = r.db.QueryRow(ctx, query).Scan(&id, &hash, &role)
+	return
+}
